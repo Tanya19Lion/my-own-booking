809c56cea5188944b020588783e1889a445164cb
Tanya19Lion <tanyatriteknitskaya@gmail.com>
2025-05-21 21:09:16 +0300

    changed some configs

--- src/lib/cloudinary.ts
+++ src/lib/cloudinary.ts
@@ -1,9 +1,9 @@
 import { v2 as cloudinary } from "cloudinary";
 
 cloudinary.config({
-    cloud_name: 'djfrvng3e', 
-    api_key: '719642979919148', 
-    api_secret: '_cbzPm0MX_VunC5Oninh-jtXR7s' // Click 'View API Keys' above to copy your API secret
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 export default cloudinary;
